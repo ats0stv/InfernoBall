99ed26d1461caa2fc75b0a54feb627e6bd0c0da4
Arun Thundyill Saseendran <ats0stv@gmail.com>
2018-11-03 00:06:44 +0000

    Updated secrets

--- Scripts/automate.sh
+++ Scripts/automate.sh
@@ -1,4 +1,4 @@
-python preparePotForSubmission.py -d ../Hashes/Level5/pots/ -o team8_l5.out
-mv -f team8_l5.pot ../Hashes/Level5/pots/
-python ghost.py -p ../Hashes/Level5/pots/team8_l5.pot -i ../00076-tsaregod-l5.as5 -s ../team08.secrets -o ../00076-tsaregod-l6.as5
+python preparePotForSubmission.py -d ../Hashes/Level7/pots/ -o team8_l7.out
+mv -f team8_l7.pot ../Hashes/Level7/pots/
+python ghost.py -p ../Hashes/Level7/pots/team8_l7.pot -i ../00076-tsaregod-l7.as5 -s ../team08.secrets -o ../00076-tsaregod-l8.as5
 python viren.py
