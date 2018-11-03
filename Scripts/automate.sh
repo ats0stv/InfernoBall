74f9068520497ea588dab971fe306319186579e0
Arun Thundyill Saseendran <ats0stv@gmail.com>
2018-11-03 11:29:22 +0000

    Update for l8

--- Scripts/automate.sh
+++ Scripts/automate.sh
@@ -1,4 +1,4 @@
-python preparePotForSubmission.py -d ../Hashes/Level7/pots/ -o team8_l7.out
-mv -f team8_l7.pot ../Hashes/Level7/pots/
-python ghost.py -p ../Hashes/Level7/pots/team8_l7.pot -i ../00076-tsaregod-l7.as5 -s ../team08.secrets -o ../00076-tsaregod-l8.as5
+python preparePotForSubmission.py -d ../Hashes/Level8/pots/ -o team8_l8.out
+mv -f team8_l8.pot ../Hashes/Level8/pots/
+python ghost1.py -p ../Hashes/Level8/pots/team8_l8.pot -i ../00076-tsaregod-l8.as5 -s ../team08.secrets -o ../00076-tsaregod-l9.as5
 python viren.py
